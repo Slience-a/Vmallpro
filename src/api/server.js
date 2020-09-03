f73b04d941ec176ea3542ee3be7e30e16ee0f823
Nz1905-2 <1730604617@qq.com>
2020-09-03 10:35:39 +0800

    详情页的开发

--- src/api/server.js
+++ src/api/server.js
@@ -4,7 +4,7 @@
 define (['jquery'],function($){
 
     function getBannerData(){
-           return $.ajax('/api/mock/banner.json');//提供出去一个promise对象
+        return $.ajax('/api/mock/banner.json');//提供出去一个promise对象
      }
      function getBanner2Data(){
         return $.ajax('/api/mock/banner2.json');//提供出去一个promise对象
@@ -18,11 +18,24 @@ define (['jquery'],function($){
      function getPadData(){
          return $.ajax('/api/mock/pad.json');
      }
+     function getDetailData(type,id){
+         var promise = new Promise(function(resolve,reject){
+            $.ajax(`/api/mock/${type}.json`).then((res)=>{
+                for(var i = 0;i < res.goods_list.length;i++){
+                    if(id == res.goods_list[i].goodsId){
+                        resolve(res.goods_list[i]);//找到id对应的数据
+                    }
+                }
+         });
+    });
+               return promise;
+     }
       return{
         getBannerData,
         getBanner2Data,
         getPhoneData,
         getBookData,
-        getPadData
+        getPadData,
+        getDetailData
       };
 });
